d0d26dcd9bc48403af742043a746b67a55b4dd8b
james <xtreme.maicolxt94@gmail.com>
2019-02-15 04:44:39 -0500

    Added dispose methods

--- dotnet4/delegateWrapper.cs
+++ dotnet4/delegateWrapper.cs
@@ -7,15 +7,44 @@ namespace jxshell.dotnet4
 	public class delegateWrapper : wrapper
 	{
 		public object __internalTarget;
-
+        public bool disposed;
 		public string __internalMethod = "";
 
 		public delegateWrapper()
 		{
 		}
 
+
+        public object target
+        {
+            get
+            {
+                return __internalTarget;
+            }
+        }
+
+
+
+        public override void dispose()
+        {
+
+            disposed = true;
+            // try call destroy 
+            try
+            {
+                invoker inv = new invoker();
+                inv.invokeMethod(__internalTarget, "destroy", new object[0]);
+            }
+            catch (Exception)
+            {
+            }
+
+            this.__internalTarget = null;
+            wrappedObject = null;
+        }
 		public delegateWrapper(object o) : base(o)
 		{
+            
 		}
 
 		public delegateWrapper(object o, typeDescriptor td) : base(o, td)
